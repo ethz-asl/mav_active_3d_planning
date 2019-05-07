d8ff666a5c8dbfeffa921156da4d887c83a48134
Lukas Schmid <schmluk@student.ethz.ch>
2019-05-07 16:35:21 +0200

    outdoor testing

--- mav_active_3d_planning/scripts/run_experiment_series_multi.sh
+++ mav_active_3d_planning/scripts/run_experiment_series_multi.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # *** Args (need to be set) ***
-n_experiments=1
-target_dir="/home/lukas/Documents/MT/Data/cost_dist_rel"		# Can reuse same dir to add experiments
+n_experiments=6
+target_dir="/home/lukas/Documents/MT/Data/costs_exp_length"		# Can reuse same dir to add experiments
 clear_voxblox_maps=true		# Irreversibly remove maps after evaluation to save disk space
 experiment=1		# For auto configs of exp1/2 (city/windmill)
 
@@ -40,3 +40,24 @@ do
 done
 
 roslaunch mav_active_3d_planning evaluate_experiment.launch target_directory:=$target_dir series:=true 
+
+
+
+
+# *** Run 2nd exp ***
+target_dir="/home/lukas/Documents/MT/Data/costs_exp_time"		# Can reuse same dir to add experiments
+planner_cfg="planners/costs2.yaml"
+
+if [ ! -d "$target_dir" ]; then
+  mkdir $target_dir
+fi
+for (( i=1; i<=n_experiments; i++ ))
+do  
+  # run experiment
+  roslaunch mav_active_3d_planning run_experiment.launch data_directory:=$target_dir record_data:=true experiment_config:=$cfg data_frequency:=$freq time_limit:=$dur planner_general_config:=$planner_cfg
+   roslaunch mav_active_3d_planning evaluate_experiment.launch target_directory:=$target_dir method:=recent series:=false clear_voxblox_maps:=$clear_voxblox_maps gt_file_path:=$pcl evaluate:=true
+done
+
+roslaunch mav_active_3d_planning evaluate_experiment.launch target_directory:=$target_dir series:=true 
+
+
