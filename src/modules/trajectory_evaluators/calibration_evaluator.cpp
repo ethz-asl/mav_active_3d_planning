044ed3b37a80853c21d8c310d9eec9e16442ccc0
Yannick Huber <huberya@student.ethz.ch>
2019-08-07 11:36:49 +0200

    these changes need to be applied to devel

--- src/modules/trajectory_evaluators/calibration_evaluator.cpp
+++ src/modules/trajectory_evaluators/calibration_evaluator.cpp
@@ -33,9 +33,8 @@ namespace mav_active_3d_planning {
                 return false;
             }
             if(traj_in->parent && trajIdMap.find(traj_in->parent) == trajIdMap.end()){
-                ROS_WARN("Attempting to evaluate child before parent, skipping!");
-                traj_in->gain = std::numeric_limits<double>::min();
-                return false;
+                ROS_WARN("Attempting to evaluate child before parent, evaluating parent first!");
+                computeGainFromVisibleVoxels(traj_in->parent);
             }
             trajIdMap.emplace(traj_in, idCounter++);
 
