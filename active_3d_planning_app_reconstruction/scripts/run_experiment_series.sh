0108b82bf0bbefe0627069d4ab48414c03b46dd7
Schmluk <schmluk@student.ethz.ch>
2020-09-18 13:16:48 +0200

    run experiments series

--- active_3d_planning_app_reconstruction/scripts/run_experiment_series.sh
+++ active_3d_planning_app_reconstruction/scripts/run_experiment_series.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # *** Args (need to be set) ***
-n_experiments=10
-target_dir="/home/lukas/Documents/MT/Data/exp2_spiral"		# Can reuse same dir to add experiments
+n_experiments=15
+target_dir="/home/lukas/Documents/Glocal/active_3d/no_drift"		# Can reuse same dir to add experiments
 clear_voxblox_maps=true		# Irreversibly remove maps after evaluation to save disk space
-experiment=2		# For auto configs of exp1/2 (city/windmill)
+experiment=1		# For auto configs of exp1/2 (city/windmill)
 
 
 # *** Run experiments ***
@@ -17,9 +17,9 @@ fi
 
 # Use auto config for experiments (to make sure nothing can go wrong!)
 if [[ $experiment == 1 ]]; then
-  cfg="experiment1.yaml"
-  freq=30
-  dur=30
+  cfg="Maze.yaml"
+  freq=20
+  dur=20
   pcl="/home/lukas/Documents/MT/experiment1/gt_surface_pcl.ply"
 elif [[ $experiment == 2 ]]; then
   cfg="experiment2.yaml"
@@ -32,11 +32,28 @@ fi
 for (( i=1; i<=n_experiments; i++ ))
 do  
   # run experiment
-#  roslaunch mav_active_3d_planning run_experiment.launch data_directory:=$target_dir record_data:=true experiment_config:=$cfg data_frequency:=$freq time_limit:=$dur
-  # run spiral
-   roslaunch mav_active_3d_planning run_spiral.launch data_directory:=$target_dir
+  roslaunch active_3d_planning_app_reconstruction run_glocal.launch data_directory:=$target_dir record_data:=true experiment_config:=$cfg data_frequency:=$freq time_limit:=$dur
+
   # evaluate
-   roslaunch mav_active_3d_planning evaluate_experiment.launch target_directory:=$target_dir method:=recent series:=false clear_voxblox_maps:=$clear_voxblox_maps gt_file_path:=$pcl evaluate:=true
+   roslaunch active_3d_planning_app_reconstruction evaluate_experiment.launch target_directory:=$target_dir method:=recent series:=false clear_voxblox_maps:=$clear_voxblox_maps gt_file_path:=$pcl evaluate:=true
 done
 
-roslaunch mav_active_3d_planning evaluate_experiment.launch target_directory:=$target_dir series:=true 
+roslaunch active_3d_planning_app_reconstruction evaluate_experiment.launch target_directory:=$target_dir series:=true 
+
+
+# Second set
+target_dir="/home/lukas/Documents/Glocal/active_3d/drift"		# Can reuse same dir to add experiments
+
+# Run the experiments
+for (( i=1; i<=n_experiments; i++ ))
+do  
+  # run experiment
+  roslaunch active_3d_planning_app_reconstruction run_glocal.launch data_directory:=$target_dir record_data:=true experiment_config:=$cfg data_frequency:=$freq time_limit:=$dur airsim_config:=maze_airsim_drift.yaml
+
+  # evaluate
+   roslaunch active_3d_planning_app_reconstruction evaluate_experiment.launch target_directory:=$target_dir method:=recent series:=false clear_voxblox_maps:=$clear_voxblox_maps gt_file_path:=$pcl evaluate:=true
+done
+
+roslaunch active_3d_planning_app_reconstruction evaluate_experiment.launch target_directory:=$target_dir series:=true 
+
+
