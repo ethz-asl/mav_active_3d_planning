14d4bade9c68258a78a7c4f7fe4ec151e7ceca51
Lukas Schmid <schmluk@mavt.ethz.ch>
2022-07-19 15:22:46 +0200

    remove debug text

--- active_3d_planning_core/src/module/trajectory_generator.cpp
+++ active_3d_planning_core/src/module/trajectory_generator.cpp
@@ -28,28 +28,26 @@ void TrajectoryGenerator::setupFromParamMap(Module::ParamMap* param_map) {
 }
 
 bool TrajectoryGenerator::checkTraversable(const Eigen::Vector3d& position) {
-  // check bounding volume
+  // check bounding volume.
   if (!bounding_volume_->contains(position)) {
-    std::cout << "Out of bounds." << std::endl;
     return false;
   }
-  //
+
+  // Check observed map.
   if (planner_.getMap().isObserved(position)) {
     bool result = planner_.getMap().isTraversable(position);
-    std::cout << "Observed and " << (result ? "traversable" : "intraversable")
-              << std::endl;
     return result;
   }
+
+  // Check clearing radius.
   if (p_clearing_radius_ > 0.0) {
     if ((planner_.getCurrentPosition() - position).norm() <
         p_clearing_radius_) {
-      std::cout << "Unobserved but clearing." << std::endl;
       return true;
     }
   }
-  std::cout << "Unobserved and "
-            << (p_collision_optimistic_ ? "optimistic" : "conservative")
-            << std::endl;
+
+  // Optimism about unknown space.
   return p_collision_optimistic_;
 }
 
